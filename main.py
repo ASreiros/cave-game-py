ccb7dd27d50e92bbcb0d6f86d0c5043ac2307725
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2023-02-21 19:27:49 +0200

    update to avoid lower case error

--- main.py
+++ main.py
@@ -4,43 +4,43 @@ failed_instructions = "You failed to follow simple game instructions and lost. G
 
 def game():
     sword = False
-    choice = input("You are at the cave entrance. Please type wait to wait(type W) at the cave entrance, or E to enter the cave\n")
-    if choice == "W":
+    choice = input("You are at the cave entrance. Please type wait to wait(type W) at the cave entrance, or E to enter the cave\n").lower()
+    if choice == "w":
         print("My mistake, you are not in fact an adventurer. Have a good day. Game Over")
         return
-    elif choice == "E":
+    elif choice == "e":
         print("You enter the cave. It is dark in here. The cave goes deeper and deeper and finally you find yourself on the tunnel separation.\n ")
     else:
         print(failed_instructions)
         return
 
-    choice = input("Do you want to go left(type L) or right (type R)?\n ")
-    if choice == "L":
+    choice = input("Do you want to go left(type L) or right (type R)?\n ").lower()
+    if choice == "l":
         print("Left tunnel leads deeper and deeper and finally you fall into the hole. Game Over")
         return
-    elif choice == "R":
-        choice2 = input("Right tunnel leads further. You see something stuck in the wall. Do you want to try to take it out(Type T) or continue (type C) to go deeper?\n")
-        if choice2 == "T":
+    elif choice == "r":
+        choice2 = input("Right tunnel leads further. You see something stuck in the wall. Do you want to try to take it out(Type T) or continue (type C) to go deeper?\n").lower()
+        if choice2 == "t":
             print("It is a sword. You take the sword and continue your journey")
             sword = True
-        elif choice2 != "C":
+        elif choice2 != "c":
             print(failed_instructions)
             return
     else:
         print(failed_instructions)
         return
-    choice = input("You arrive to the big cave with lots of gold. It is guarded by the dragon. You can try to hide(type H), run(type R) or fight(type F) the Dragon?\n")
+    choice = input("You arrive to the big cave with lots of gold. It is guarded by the dragon. You can try to hide(type H), run(type R) or fight(type F) the Dragon?\n").lower()
 
-    if choice == "H":
+    if choice == "h":
         print("Dragon found you and ate you. Game over")
         return
-    elif choice == "R":
+    elif choice == "r":
         print("Dragon catch you and ate you. Game over")
         return
-    elif choice == "F" and sword is False:
+    elif choice == "f" and sword is False:
         print("It was a short fight. Dragon ate you")
         return
-    elif choice == "F" and sword is True:
+    elif choice == "f" and sword is True:
         print("You manage to win the fight and slay the Dragon. The treasure is yours. You win.")
         return
     else:
